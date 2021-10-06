e4c40065b03ad5f1b50a2820e236a4e0a628996f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 08:45:33 +0900

    とりあえずオペレーションコードの定義を放り込んでみた

---
+++ Client/AnpanOnline/Assets/Scripts/Share/OpCode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnpanGameCommon
+{
+    /// <summary>
+    /// オペレーションコード
+    /// </summary>
+    public enum EOpCode : byte {
+        LogIn,
+    }
+}
