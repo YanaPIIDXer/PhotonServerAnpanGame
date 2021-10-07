59e202a38c6705f9e22a756e3c0bed34a7461965
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 14:15:23 +0900

    受信したリクエストをcurrentStateに投げるようにした

--- Server/AnpanOnline/PeerState/PeerStateTitle.cs
+++ Server/AnpanOnline/PeerState/PeerStateTitle.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Photon.SocketServer;
+using AnpanGameCommon;
 
 namespace AnpanOnline.PeerState
 {
@@ -15,6 +17,21 @@ namespace AnpanOnline.PeerState
 		public PeerStateTitle(GamePeer parent)
 			: base(parent)
 		{
+			AddOperationHandler(EOpCode.LogIn, LogInHandler);
+		}
+
+		/// <summary>
+		/// ログインオペレーションハンドラ
+		/// </summary>
+		/// <param name="request">リクエスト</param>
+		/// <returns>レスポンス</returns>
+		private OperationResponse LogInHandler(OperationRequest request)
+		{
+			return new OperationResponse()
+			{
+				OperationCode = (byte)EOpCode.LogIn,
+				Parameters = new Dictionary<byte, object>() { { 0, "OK" } }
+			};
 		}
 	}
 }
