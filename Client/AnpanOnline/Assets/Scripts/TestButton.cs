c1a79a15499014bf5284db35b137799d455049e4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 14:13:46 +0900

    ・テスト用コード復活 ・ゲームの設定値を定義するstaticクラスを用意して、サーバホストをそこに定義した

---
+++ Client/AnpanOnline/Assets/Scripts/TestButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PhotonServerClient;
+using UniRx;
+using System;
+using ExitGames.Client.Photon;
+using Cysharp.Threading.Tasks;
+using AnpanGameCommon;
+using Game;
+
+public class TestButton : MonoBehaviour
+{
+    void Awake()
+    {
+        var button = GetComponent<Button>();
+        button.OnClickAsObservable()
+              .Subscribe(async _ =>
+              {
+                  var client = new PhotonClient();
+                  var token = this.GetCancellationTokenOnDestroy();
+                  try
+                  {
+                      await client.Connect(GameConfigure.ServerHost, "AnpanOnline", ConnectionProtocol.Tcp, token);
+                      Debug.Log("Connected!");
+
+                      await UniTask.Delay(1000);
+
+                      var response = await client.SendOperationRequest((byte)EOpCode.LogIn, new Dictionary<byte, object>(), (byte)EOpCode.LogIn, token);
+                      Debug.Log("Response OK!");
+                      Debug.Log("Message:" + response[0]);
+
+                      await UniTask.Delay(1000);
+                  }
+                  catch (Exception e)
+                  {
+                      Debug.LogError(e.Message);
+                  }
+                  finally
+                  {
+                      client.Disconnect();
+                      Debug.Log("Disconnect.");
+                  }
+              }).AddTo(gameObject);
+    }
+}
