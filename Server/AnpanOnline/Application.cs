7ed3ee1d77c66b6bd1906c97b29e740f35a0ab5c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 12:03:20 +0900

    最低限のクラスを定義

---
+++ Server/AnpanOnline/Application.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Photon.SocketServer;
+
+namespace AnpanOnline
+{
+	/// <summary>
+	/// アプリケーションクラス
+	/// </summary>
+	public class Application : ApplicationBase
+	{
+		/// <summary>
+		/// Peerの生成
+		/// </summary>
+		/// <param name="initRequest">初期化情報</param>
+		/// <returns>Peerのインスタンス</returns>
+		protected override PeerBase CreatePeer(InitRequest initRequest)
+		{
+			return new GamePeer(initRequest);
+		}
+
+		/// <summary>
+		/// 初期化処理
+		/// </summary>
+		protected override void Setup()
+		{
+		}
+
+		/// <summary>
+		/// 終了処理
+		/// </summary>
+		protected override void TearDown()
+		{
+		}
+	}
+}
