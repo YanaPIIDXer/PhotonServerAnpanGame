ccaa60ea558c48aad01304146dee5a949635891b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 16:38:30 +0900

    新規登録ボタン or ログインボタン押下時に入力された情報をとりあえずログに吐き出すようにした

--- Client/AnpanOnline/Assets/Scripts/Sequence/TitleSequence.cs
+++ Client/AnpanOnline/Assets/Scripts/Sequence/TitleSequence.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UI.Title;
+using UniRx;
+using System;
 
 namespace Sequence
 {
@@ -9,5 +12,21 @@ namespace Sequence
     /// </summary>
     public class TitleSequence : MonoBehaviour
     {
+        /// <summary>
+        /// タイトル画面
+        /// </summary>
+        [SerializeField]
+        private TitleScreen titleScreen = null;
+
+        void Awake()
+        {
+            titleScreen.OnRegister
+                       .Subscribe(info => Debug.Log("Register Email:" + info.EMailAddress))
+                       .AddTo(gameObject);
+
+            titleScreen.OnLogIn
+                       .Subscribe(info => Debug.Log("LogIn Email:" + info.EMailAddress))
+                       .AddTo(gameObject);
+        }
     }
 }
