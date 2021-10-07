e9c785e37e3f01776051a99edd51e17e6286218d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 16:50:23 +0900

    新規登録・ログイン処理実装

--- Client/AnpanOnline/Assets/Scripts/Sequence/TitleSequence.cs
+++ Client/AnpanOnline/Assets/Scripts/Sequence/TitleSequence.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UI.Title;
 using UniRx;
 using System;
+using Firebase.Auth;
 
 namespace Sequence
 {
@@ -21,12 +22,52 @@ namespace Sequence
         void Awake()
         {
             titleScreen.OnRegister
-                       .Subscribe(info => Debug.Log("Register Email:" + info.EMailAddress))
+                       .Subscribe(info => Register(info))
                        .AddTo(gameObject);
 
             titleScreen.OnLogIn
-                       .Subscribe(info => Debug.Log("LogIn Email:" + info.EMailAddress))
+                       .Subscribe(info => LogIn(info))
                        .AddTo(gameObject);
+        }
+
+        /// <summary>
+        /// 新規登録
+        /// </summary>
+        /// <param name="info">認証情報</param>
+        private async void Register(TitleScreen.LoginInfo info)
+        {
+            FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+            try
+            {
+                var user = await auth.CreateUserWithEmailAndPasswordAsync(info.EMailAddress, info.Password);
+                var token = await user.TokenAsync(true);
+                Debug.Log("Register User Token:" + token);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                titleScreen.OnProcessError();
+            }
+        }
+
+        /// <summary>
+        /// ログイン
+        /// </summary>
+        /// <param name="info">認証情報</param>
+        private async void LogIn(TitleScreen.LoginInfo info)
+        {
+            FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+            try
+            {
+                var user = await auth.SignInWithEmailAndPasswordAsync(info.EMailAddress, info.Password);
+                var token = await user.TokenAsync(true);
+                Debug.Log("LogIn User Token:" + token);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                titleScreen.OnProcessError();
+            }
         }
     }
 }
