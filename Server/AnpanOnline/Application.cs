f56247b3e878e30230218acd798362b18025bced
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 13:03:00 +0900

    プロジェクトを.NET Framework向けのクラスライブラリプロジェクトとして作り直した

---
+++ Server/AnpanOnline/Application.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+using Photon.SocketServer;
+
+namespace AnpanOnline
+{
+	/// <summary>
+	/// アプリケーションクラス
+	/// </summary>
+	public class Application : ApplicationBase
+	{
+		/// <summary>
+		/// Peerの生成
+		/// </summary>
+		/// <param name="initRequest">初期化情報</param>
+		/// <returns>Peerのインスタンス</returns>
+		protected override PeerBase CreatePeer(InitRequest initRequest)
+		{
+			return new GamePeer(initRequest);
+		}
+
+		/// <summary>
+		/// 初期化処理
+		/// </summary>
+		protected override void Setup()
+		{
+			SetupLog();
+			LogManager.GetLogger("").Debug("Anpan Online Server Setup OK.");
+		}
+
+		/// <summary>
+		/// ログのセットアップ
+		/// </summary>
+		private void SetupLog()
+		{
+			log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationRootPath, "log");
+			var configFileInfo = new FileInfo(Path.Combine(this.BinaryPath, "log4net.config"));
+			if (configFileInfo.Exists)
+			{
+				LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+				XmlConfigurator.ConfigureAndWatch(configFileInfo);
+			}
+		}
+
+		/// <summary>
+		/// 終了処理
+		/// </summary>
+		protected override void TearDown()
+		{
+		}
+	}
+}
---
+++ Server/AnpanOnline/GamePeer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using AnpanGameCommon;
+
+namespace AnpanOnline
+{
+	/// <summary>
+	/// Peer
+	/// </summary>
+	public class GamePeer : ClientPeer
+	{
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="initRequest">初期化情報</param>
+		public GamePeer(InitRequest initRequest)
+			: base(initRequest)
+		{
+		}
+
+		/// <summary>
+		/// 切断された
+		/// </summary>
+		/// <param name="reasonCode">切断理由</param>
+		/// <param name="reasonDetail">詳細</param>
+		protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+		{
+		}
+
+		/// <summary>
+		/// オペレーションを受信した
+		/// </summary>
+		/// <param name="operationRequest">リクエスト</param>
+		/// <param name="sendParameters">送信パラメータ</param>
+		protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+		{
+			switch((EOpCode)operationRequest.OperationCode)
+			{
+				case EOpCode.LogIn:
+				default:
+
+					{
+						OperationResponse response = new OperationResponse
+						{
+							ReturnCode = 0,
+							Parameters = new Dictionary<byte, object> { { 0, "OK!" } }
+						};
+						SendOperationResponse(response, sendParameters);
+					}
+					break;
+			}
+		}
+	}
+}
