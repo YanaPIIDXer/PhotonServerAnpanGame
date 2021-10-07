ce6100b8cfe0143d2f1d5c288e923098b65ce4dc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 09:13:27 +0900

    必要最低限のクラスを定義

---
+++ Server/ServerCore/GamePeer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace ServerCore
+{
+	public class GamePeer : ClientPeer
+	{
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="initRequest">初期化情報</param>
+		public GamePeer(InitRequest initRequest)
+			: base(initRequest)
+		{
+		}
+
+		/// <summary>
+		/// 切断された
+		/// </summary>
+		/// <param name="reasonCode">切断理由</param>
+		/// <param name="reasonDetail">詳細</param>
+		protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+		{
+		}
+
+		/// <summary>
+		/// オペレーション受信
+		/// </summary>
+		/// <param name="operationRequest">リクエスト</param>
+		/// <param name="sendParameters">パラメータ</param>
+		protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+		{
+		}
+	}
+}
