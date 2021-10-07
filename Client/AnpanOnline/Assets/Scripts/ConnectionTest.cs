08e8c60665a0edb7e80dc375cd3ded7aa62889b5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 09:43:14 +0900

    とりあえず接続するだけの実験

---
+++ Client/AnpanOnline/Assets/Scripts/ConnectionTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using PhotonServerClient;
+using ExitGames.Client.Photon;
+
+/// <summary>
+/// 実験用コード
+/// 後で消す
+/// </summary>
+public class ConnectionTest : MonoBehaviour
+{
+    private PhotonClient client = new PhotonClient();
+
+    void Awake()
+    {
+        var button = GetComponent<Button>();
+        button.OnClickAsObservable()
+              .Subscribe(async _ =>
+              {
+                  var token = this.GetCancellationTokenOnDestroy();
+                  button.interactable = false;
+                  try
+                  {
+                      await client.Connect("127.0.0.1:4530", "AnpanOnline", ConnectionProtocol.Tcp, token);
+                      Debug.Log("Connection Success!");
+                      await UniTask.Delay(1000);
+                  }
+                  catch (Exception e)
+                  {
+                      Debug.LogError(e.Message);
+                  }
+                  finally
+                  {
+                      client.Disconnect();
+                      Debug.Log("Disconnect");
+                      button.interactable = true;
+                  }
+              }).AddTo(gameObject);
+    }
+}
