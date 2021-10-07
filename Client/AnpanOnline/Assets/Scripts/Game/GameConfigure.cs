0e3bdaa2771170825f46a3b9c807fc087c13823a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 14:24:19 +0900

    消してはいけないものを消していたので復活させた

---
+++ Client/AnpanOnline/Assets/Scripts/Game/GameConfigure.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public static class GameConfigure
+    {
+        /// <summary>
+        /// サーバホスト
+        /// </summary>
+        public static readonly string ServerHost = "127.0.0.1:4530";
+    }
+}
