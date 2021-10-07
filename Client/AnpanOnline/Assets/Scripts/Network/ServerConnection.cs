65ee2c575d415c5bddfdff7014d006bc21fe4868
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 17:43:36 +0900

    PhotonClientをSingleton化する為のクラスを定義

---
+++ Client/AnpanOnline/Assets/Scripts/Network/ServerConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PhotonServerClient;
+
+namespace Network
+{
+    /// <summary>
+    /// サーバコネクション
+    /// ※PhotonClientをSingleton化するためのクラス
+    /// </summary>
+    public static class ServerConnection
+    {
+        private static PhotonClient instance = new PhotonClient();
+        public static PhotonClient Instance { get { return instance; } }
+    }
+}
