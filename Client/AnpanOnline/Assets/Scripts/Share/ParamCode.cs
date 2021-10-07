a20df123f4bc8240b4237cb6d07fe3fdcd2720ac
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 17:24:51 +0900

    パラメータコードを定義するenum（staticクラスにbyte型の定数を定義してるだけ）を追加

---
+++ Client/AnpanOnline/Assets/Scripts/Share/ParamCode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnpanGameCommon
+{
+    /// <summary>
+    /// パラメータコード
+    /// </summary>
+    public static class EParamCode
+    {
+        // LogIn Request
+        public static readonly byte AuthToken = 0;
+
+        // LogIn Response
+        public static readonly byte VerifyResult = 0;
+    }
+}
