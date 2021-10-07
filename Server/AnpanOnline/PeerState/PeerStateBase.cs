e7afd0d5ae7db216620a31a0d7f92e5be12cc67f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 13:58:50 +0900

    オペレーションリクエストを処理するハンドラの仕組みを構築

--- Server/AnpanOnline/PeerState/PeerStateBase.cs
+++ Server/AnpanOnline/PeerState/PeerStateBase.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AnpanGameCommon;
+using Photon.SocketServer;
 
 namespace AnpanOnline.PeerState
 {
@@ -24,5 +26,38 @@ namespace AnpanOnline.PeerState
 		/// 親
 		/// </summary>
 		protected GamePeer Parent { get; private set; }
+
+		/// <summary>
+		/// オペレーションハンドラ
+		/// </summary>
+		private Dictionary<EOpCode, Func<OperationRequest, OperationResponse>> operationHandlers = new Dictionary<EOpCode, Func<OperationRequest, OperationResponse>>();
+
+		/// <summary>
+		/// オペレーションコードに対応したハンドラの追加
+		/// ※ハンドラはオペレーションに対するレスポンスを返す
+		/// 　レスポンスが無い場合はnullを返す事
+		/// </summary>
+		/// <param name="opCode">オペレーションコード</param>
+		/// <param name="handler">ハンドラ</param>
+		protected void AddOperationHandler(EOpCode opCode, Func<OperationRequest, OperationResponse> handler)
+		{
+			operationHandlers[opCode] = handler;
+		}
+
+		public void OnOperation(OperationRequest request)
+		{
+			EOpCode opCode = (EOpCode)request.OperationCode;
+			if (!operationHandlers.ContainsKey(opCode))
+			{
+				Logger.Log.WarnFormat("OperationCode:{0} is not handled.", opCode.ToString());
+				return;
+			}
+
+			OperationResponse response = operationHandlers[opCode](request);
+			if (response != null)
+			{
+				Parent.SendOperationResponse(response, new SendParameters());
+			}
+		}
 	}
 }
